e313d111c469e6bc5cb0b042f527d497521a6be5
Gerihh <horvathgergo1208@gmail.com>
2024-02-13 13:56:51 +0100

    clear frontend

---
+++ src/pages/CreatorsPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <q-page>
+    <div class="column flex-center" style="min-height: inherit">
+      <h1>Horváth Gergő</h1>
+    </div>
+  </q-page>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 3vw;
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,21 +7,11 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   {
-    path: "/empty",
+    path: "/creators",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/EmptyPage.vue") }],
+    children: [{ path: "", component: () => import("pages/CreatorsPage.vue") }],
   },
-  {
-    path: "/basic",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/BasicPage.vue") }],
-  },
-  {
-    path: "/table",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/TablePage.vue") }],
-  },
-  
+
   // Always leave this as last one,
   // but you can also remove it
   {
