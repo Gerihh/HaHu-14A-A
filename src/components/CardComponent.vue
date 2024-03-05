c1dc3af8acdb8d3189b77cf3f0630afc75e8cc7a
Gerihh <horvathgergo1208@gmail.com>
2024-03-05 08:52:48 +0100

    card component

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,40 +1,88 @@
-<template>
-  <div class="q-pa-md row items-start q-gutter-md q-ma-lg">
-    <q-card class="q-ma-lg">
-      <q-card-section style="background-color: rgb(200, 190, 156)">
-        <div>{{ id }}</div>
-      </q-card-section>
-      <q-card-section style="background-color: rgb(255, 228, 196)">
-        <div></div>
-      </q-card-section>
-      <q-card-section style="background-color: rgb(200, 190, 156)">
-        <div></div>
-      </q-card-section>
-      <q-card-section style="background-color: rgb(255, 228, 196)">
-        <img src="asd" />
-      </q-card-section>
-      <q-card-section style="background-color: rgb(200, 190, 156)">
-        <div class="text-h6"></div>
-      </q-card-section>
-      <q-card-section style="background-color: rgb(255, 228, 196); height: 50px">
-        <q-btn class="bg-green-3 absolute-center">Hirdetés szerkesztése</q-btn>
-      </q-card-section>
-    </q-card>
-  </div>
-</template>
-
-<script setup lang="ts">
-import axios from "axios";
+<script>
 import { ref, onMounted } from "vue";
+import { useStore } from "src/stores/store";
 
-const id = ref();
+const store = useStore();
 
-onMounted(async () => {
-  try {
-    const response = await axios.get("http://localhost:3000/advertisements");
-    id.value = response.data.id;
-  } catch (error) {
-    console.error("Error fetching advertisements:", error);
-  }
+onMounted(() => {
+  store.getAllCategories();
+  store.one_GetByCategory(selectedCategoryName?.value?.categoryNameField || "Személyautó");
 });
+
+let displayText = ref("");
+export default {
+  props: ["index"],
+  setup(props) {
+    console.log(props.index);
+    const longText = ref(store.many.documents[props.index].leiras);
+    const toggled = ref(false);
+    const handleToggle = (toggled) => {
+      if (!toggled) {
+        let slicedText = "";
+        let lastWhiteSpaceIdx = -1;
+        for (let index = 0; index < 100; index++) {
+          slicedText += longText.value[index];
+          if (longText.value[index] == " ") {
+            lastWhiteSpaceIdx = index;
+          }
+        }
+        if (slicedText[99] == " ") {
+          displayText.value = slicedText.slice(0, 98) + "...";
+        } else {
+          displayText.value = slicedText.slice(0, lastWhiteSpaceIdx) + "...";
+        }
+      } else {
+        displayText.value = longText.value;
+      }
+    };
+
+    return {
+      longText,
+      toggled,
+      displayText,
+      handleToggle,
+    };
+  },
+
+  mounted() {
+    this.handleToggle();
+  },
+};
 </script>
+
+<template>
+  <q-card bordered class="q-ma-md" flat>
+    <q-card-section class="text-center text-h5" style="background-color: rgb(200, 190, 156)">
+      Our Changing Planet
+    </q-card-section>
+    <q-card-section class="text-h7" style="background-color: rgb(255, 228, 196)">
+      <ul>
+        <li><span>Szin: </span><b>template</b></li>
+        <li><span>Évjárat: </span><b>template</b></li>
+        <li><span>Hengerűrtartalom: </span><b>template</b></li>
+        <li><span>Hirdetés dátuma: </span><b>template</b></li>
+      </ul>
+    </q-card-section>
+    <q-card-section class="" style="background-color: rgb(200, 190, 156)">
+      <div class="text-h7 text-justify">{{ displayText }}</div>
+      <hr />
+      <q-toggle
+        v-model="toggled"
+        color="dark-blue"
+        :disable="longText.length <= 100"
+        label="Teljes hirdetés"
+        @update:model-value="handleToggle"
+      />
+    </q-card-section>
+    <q-card-section style="background-color: rgb(255, 228, 196)">
+      <q-img role="img" src="../assets/logo.png" style="max-height: 200px"></q-img>
+    </q-card-section>
+    <q-card-section style="background-color: rgb(200, 190, 156)">
+      <div class="text-h7 text-justify">link</div>
+    </q-card-section>
+    <q-card-actions class="justify-center" style="background-color: rgb(255, 228, 196)">
+      <q-btn class="bg-green-3" label="Hirdetés szerkesztése" no-caps type="button"></q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+<style lang="sass" scoped></style>
